9eac236980d64ae108512b8814dee2146c35b8d3
nao9824 <k023g0055@g.neec.ac.jp>
2024-04-16 16:51:17 +0900

    箱を押せるようにした

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-
     //配列の宣言
     int[] map;
+    void PrintArray()
+    {
+        string debugText = "";
+        for (int i = 0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+        Debug.Log(debugText);
+    }
+
+    int GetPlayerIndex()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    bool MoveNumber(int number,int moveFrom,int moveTo) {
+    if(moveTo < 0 || moveTo >= map.Length)
+        {
+            return false;
+        }
+        //移動先に箱がいたら
+        if (map[moveTo] == 2) { 
+        //どの方向に移動させるかを算出
+        int velocity = moveTo - moveFrom;
+        bool success = MoveNumber(2, moveTo, moveTo + velocity);
+        //もし箱が移動失敗したら、プレイヤーも移動失敗
+        if(!success) { return false; }
+        }
+        map[moveTo] = number;
+        map[moveFrom] = 0;
+        return true;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
-        //追加。文字列の宣言と初期化
-        string debugText = "";
-        for(int i=0;i<map.Length; i++)
-        {
-            debugText += map[i].ToString()+",";
-        }
-        Debug.Log(debugText);
+        map = new int[] { 0, 1, 0, 2, 0, 2, 0, 0, 0 };
+        PrintArray();
     }
 
     // Update is called once per frame
@@ -26,56 +58,20 @@ public class GameManagerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            int playerIndex = -1;
+            int playerIndex = GetPlayerIndex();
 
-            for(int i=0;i<map.Length;i++)
-            {
-                if (map[i] == 1)
-                {
-                    playerIndex = i;
-                    break;
-                }
-            }
+            MoveNumber(1, playerIndex, playerIndex + 1);
 
-            if (playerIndex < map.Length - 1)
-            {
-                map[playerIndex+1] = 1;
-                map[playerIndex] = 0;
-            }
-
-            string debugText = "";
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ",";
-            }
-            Debug.Log(debugText);
+            PrintArray();
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            int playerIndex = -1;
+            int playerIndex = GetPlayerIndex();
 
-            for (int i = 0; i < map.Length; i++)
-            {
-                if (map[i] == 1)
-                {
-                    playerIndex = i;
-                    break;
-                }
-            }
+            MoveNumber(1, playerIndex, playerIndex - 1);
 
-            if (playerIndex > 0)
-            {
-                map[playerIndex - 1] = 1;
-                map[playerIndex] = 0;
-            }
-
-            string debugText = "";
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ",";
-            }
-            Debug.Log(debugText);
+            PrintArray();
         }
     }
 }
