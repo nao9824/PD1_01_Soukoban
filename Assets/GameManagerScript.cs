bda0364e1a4cc277310c8d3cce3aed4a95a03eb6
nao9824 <k023g0055@g.neec.ac.jp>
2024-04-16 15:53:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    //”z—ñ‚ÌéŒ¾
+    int[] map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        //’Ç‰ÁB•¶š—ñ‚ÌéŒ¾‚Æ‰Šú‰»
+        string debugText = "";
+        for(int i=0;i<map.Length; i++)
+        {
+            debugText += map[i].ToString()+",";
+        }
+        Debug.Log(debugText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            int playerIndex = -1;
+
+            for(int i=0;i<map.Length;i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            if (playerIndex < map.Length - 1)
+            {
+                map[playerIndex+1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            string debugText = "";
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText += map[i].ToString() + ",";
+            }
+            Debug.Log(debugText);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            int playerIndex = -1;
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            if (playerIndex > 0)
+            {
+                map[playerIndex - 1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            string debugText = "";
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText += map[i].ToString() + ",";
+            }
+            Debug.Log(debugText);
+        }
+    }
+}
